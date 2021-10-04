67336fe32da4747760a5af960ced23838bb54fbb
Khushi Rana <91715072+KhushiRana2003@users.noreply.github.com>
2021-10-04 17:35:13 +0530

    Create unique_number_by_bit_manipution.cpp

---
+++ code/unique_number_by_bit_manipution.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+using namespace std;
+
+int unique(int arr[], int n)
+{
+    int xorsum = 0; 
+    for(int i = 0; i < n; i++)
+    {
+        xorsum = xorsum^arr[i];
+    }
+    return xorsum;
+}
+
+int main()
+{
+    int arr[5] = {4,2,3,4,2};
+
+    cout<<unique(arr,5);
+    
+    return 0;
+}
